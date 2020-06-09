2f8ed21bc80f00f0e27c9f189f09b1c8711b6e89
liyaSileshi <liyasileshi@gmail.com>
2020-06-08 23:27:44 -0700

    Updates

---
+++ precache-manifest.c8953e40e5f68dc84eb47782c3101c0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7c33ee02586885a3590607981ce7cd",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "6bbaa2b4ca1a78bf37b5",
+    "url": "/react-api/static/css/main.a2410c36.chunk.css"
+  },
+  {
+    "revision": "a04a432f3b0869574e4e",
+    "url": "/react-api/static/js/2.839df3b0.chunk.js"
+  },
+  {
+    "revision": "6bbaa2b4ca1a78bf37b5",
+    "url": "/react-api/static/js/main.773dfd8b.chunk.js"
+  },
+  {
+    "revision": "0c0307ef452a5797b067",
+    "url": "/react-api/static/js/runtime~main.a9accc2f.js"
+  }
+]);
